8111be1320109712556356ecee67a2f014b0fa25
Jinsub Ahn <jinni@Jinsubs-MacBook-Pro.local>
2016-10-08 17:34:21 +0900

    fix ignore file list.

--- setup.py
+++ setup.py
@@ -1,9 +1,6 @@
 import os
 from setuptools import setup
 
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
-
 setup(
     name = "starbucks-wifi-login-in-kor",
     version = "1.0",
